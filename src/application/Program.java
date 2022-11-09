9ec19cacc1c553ccfb874cc8ff2d0a936d2c766f
Lucas <lucasofficialcruz95@gmail.com>
2022-11-09 16:54:53 -0300

    tipos curinga

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,38 +1,25 @@
 package application;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-import entities.Product;
-import services.CalculationService;
-
 public class Program {
-
+//Com tipos curinga podemos fazer métodos 
+	// que recebem um genérico de "qualquer tipo":
 	public static void main(String[] args) {
-		List<Product> list = new ArrayList<>();
-
-		String path = "C:\\temp\\in.txt";
-
-		try (BufferedReader br = new BufferedReader(new FileReader(path))) {// abrindo a arquivo
-
-			String line = br.readLine();// ler todas as linhas do arquivo
-			while (line != null) {
-				String[] fields = line.split(",");
-				list.add(new Product(fields[0], Double.parseDouble(fields[1])));// guardando na lista de inteiros do arquivo
-				line = br.readLine();
-			}
-
-			Product x = CalculationService.max(list);//.max() vai encontrar o maior número da lista
-			System.out.println("Most expensive:");
-			System.out.println(x);
-
-		} catch (IOException e) {
-			System.out.println("Error: " + e.getMessage());
-		}
-
+		List<Integer> myInts = Arrays.asList(5, 2, 10);
+		printList(myInts);
 	}
 
+	public static void printList(List<?> list) {
+		for (Object obj : list) {
+			System.out.println(obj);
+		}
+		
+	/*public static void main(String[] args) { Porém não é possível adicionar dados a uma 
+		List<?> list = new ArrayList<Integer>(); coleção de tipo curinga
+		list.add(3); // erro de compilação
+		}*/
+	//O compilador não sabe qual é o tipo específico do qual a lista foi instanciada
+	}
 }
