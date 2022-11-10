74381ebcd4e481ea0bd19cb0379c72ccd243cf71
Lucas <lucasofficialcruz95@gmail.com>
2022-11-10 18:08:47 -0300

    HashCode e Equals

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,31 +1,15 @@
 package application;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import entities.Client;
 
 public class Program {
 	public static void main(String[] args) {
-		List<Integer> myInts = Arrays.asList(1, 2, 3, 4);
-		List<Double> myDoubles = Arrays.asList(3.14, 6.28);
-		List<Object> myObjs = new ArrayList<Object>();
+		//hashCode mostra se um Objeto é igual a outro
+		Client c1 = new Client("Maria", "maria@gmail.com");
+		Client c2 = new Client("Maria", "alex@gmail.com");
 		
-		copy(myInts, myObjs);
-		printList(myObjs);
-		copy(myDoubles, myObjs);
-		printList(myObjs);
-	}
-                        //qualquer tipo que extends number //Lista de destino é qualquer ripo de super Number 
-	public static void copy(List<? extends Number> source, List<? super Number> destiny) {
-		for (Number number : source) {//para cada number na lista de origin "source" 
-			destiny.add(number);//add na lista destiny
-		}
-	}
-	//Metodo generics para imprimir a lista 
-	public static void printList(List<?> list) {
-		for (Object obj : list) {//Lista de Object (qualquer tipo)
-			System.out.print(obj + " ");
-		}
-		System.out.println();
+		System.out.println(c1.hashCode());
+		System.out.println(c2.hashCode());
+		System.out.println(c1.equals(c2));//.equals() mostra se é igual com true/false
 	}
 }
