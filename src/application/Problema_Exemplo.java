180303be1be861e689367aafff72b7e007d8b291
Lucas <lucasofficialcruz95@gmail.com>
2022-11-16 12:57:13 -0300

    Exercício de fixação (Set

--- src/application/Problema_Exemplo.java
+++ src/application/Problema_Exemplo.java
@@ -2,10 +2,17 @@ package application;
 
 public class Problema_Exemplo {
 	/*Problema exemplo
-	Um site de internet registra um log de acessos dos usuários. Um
-	registro de log consiste no nome de usuário (apenas uma palavra) e o
-	instante em que o usuário acessou o site no padrão ISO 8601,
-	separados por espaço, conforme exemplo. Fazer um programa que leia
-	o log de acessos a partir de um arquivo, e daí informe quantos usuários
-	distintos acessaram o site.*/
+	Em um portal de cursos online, cada usuário possui um código único,
+	representado por um número inteir.
+	
+	Cada instrutor do portal pode ter vários cursos, sendo que um mesmo 
+	aluno pode se matricular em quantos cursos quiser. Assim, o número total de 
+	alunos de um instrutor não é simplesmente a soma dos alunos de todos os cursos que
+	ele possui, pois pode haver alunos repetidos em mais de um curso.
+	
+	O intrutor Alex possui três cursos A, B e C, e deseja ssaber seu número total de 
+	aluos.
+	
+	*Seu programa deve ler os alunos dos cursos A, B e C do instrutor Alex, depois 
+	mostrar a quantidade total e alunos dele, conforme exemplo.*/
 }
